d3f4eda80fb98a041fec17ce6d6308cbd8e0f723
Felipe Lopez Rios <felipetomaslopezrios@gmail.com>
2023-12-18 09:27:44 -0300

    Correccion: ID automatico e univoco

--- main.js
+++ main.js
@@ -10,28 +10,44 @@ class ProductManager {
   }
 
   addProduct(title, description, price, thumbnail, code, stock) {
+    const randomId = Math.round(Math.random() * 1000000);
     if (!title || !description || !price || !thumbnail || !code || !stock){
-      // Este condicional valida que toods los campos hayan sido completados
+      // Este condicional valida que todos los campos hayan sido completados
       console.log("Todos los campos son obligatorios.");
     } else {
-      if (!this.products.some(prod => prod.code === code)) {
-        // Este condicional valida que no exista ningun producto con codigo
-        // repetido
-        const product = { title: title, description: description, price: price, thumbnail: thumbnail, code: code, stock: stock };
-        this.products.push(product);
-      } else {
-        console.log(`Error: ya existe un producto registrado con codigo ${code}`);
-      }
+      let bandera = true
+      do {
+        // Este condicional verifica que el id generado automaticamente no se haya generado
+        // con el mismo valor que otro ya existente
+        if (!this.products.some(prod => prod.id === randomId)) {
+          // Este condicional valida que no exista ningun producto con codigo
+          // repetido
+          const product = { id: randomId, title: title, description: description, price: price, thumbnail: thumbnail, code: code, stock: stock };
+          this.products.push(product);
+        } else {
+          bandera = false;
+        }
+      } while(bandera);
     }
   }
 
-  getProductById(code) {
-    const element = this.products.find(e => e.code === code);
+  getProductById(id) {
+    const element = this.products.find(e => e.id === id);
 
     if (element){
       return element;
     } else {
-      return `No existe ningun producto con codigo ${code}`
+      return `No existe ningun producto con el ID ${id}`
     }
   }
 }
+
+let inst = new ProductManager();
+
+inst.addProduct("remera", "una remera", 999, "imagen.com", 987, 4);
+inst.addProduct("pantalon", "un pantalon", 999, "imagen.com", 986, 4);
+
+
+console.log(inst);
+
+console.log(inst.getProductById(inst.products[0].id));
